1715d6bcebcb74e753f82c473853c676d42e56fc
nour <nour@nours-MacBook-Pro.local>
2019-11-30 13:57:41 -0500

    additional setup(like class)

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'db_cancerstats',
+  port: 8889 // 
+}
+
+module.exports = config;
+
+
+// in the browser type in localhost:3000
